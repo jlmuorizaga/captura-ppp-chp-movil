184597e31a1b2bc219eae502b297f2dcd7cab093
jlmuorizaga <61767859+jlmuorizaga@users.noreply.github.com>
2024-07-14 10:03:20 -0600

    Actualiza regiones

--- src/app/catalogos/regiones/regiones-ppal/regiones-ppal.page.ts
+++ src/app/catalogos/regiones/regiones-ppal/regiones-ppal.page.ts
@@ -1,8 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/angular/standalone';
-
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons,
+   IonButton, IonIcon, IonBackButton, IonList, 
+  IonLabel, IonItem, 
+  AlertController} from '@ionic/angular/standalone';
+import { SharedModule } from 'src/app/shared/shared/shared.module';
+import {Region} from 'src/app/model/dto/region';
+import { RegionService } from 'src/app/services/region.service';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import {filter} from 'rxjs/operators';
 @Component({
   selector: 'app-regiones-ppal',
   templateUrl: './regiones-ppal.page.html',
@@ -10,11 +18,21 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton 
   standalone: true,
   imports: [IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
-export class RegionesPpalPage implements OnInit {
+export class RegionesPpalPage implements OnInit,OnDestroy {
+  navigationSubscription: Subscription;
+  regiones!:Region[];
+  mensaje: string;
 
-  constructor() { }
+  constructor(private regionesSvc:RegionService,
+    private alertController:AlertController,
+    private router: Router,private cdr: ChangeDetectorRef) { }
 
   ngOnInit() {
+    console.log('Entré a regiones en OnInit');
   }
+
+  saltaAInsertarRegion() {
+    this.router.navigateByUrl('/insertar-region');
+  }  
 
 }
