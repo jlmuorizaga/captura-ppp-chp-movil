fb5e271696d1af64784e9519d3ac952afd5423f1
jlmuorizaga <61767859+jlmuorizaga@users.noreply.github.com>
2024-07-14 12:16:07 -0600

    Update regiones-ppal.page.ts

--- src/app/catalogos/regiones/regiones-ppal/regiones-ppal.page.ts
+++ src/app/catalogos/regiones/regiones-ppal/regiones-ppal.page.ts
@@ -16,7 +16,8 @@ import {filter} from 'rxjs/operators';
   templateUrl: './regiones-ppal.page.html',
   styleUrls: ['./regiones-ppal.page.scss'],
   standalone: true,
-  imports: [IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [SharedModule,IonBackButton, IonButtons, IonContent, 
+    IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class RegionesPpalPage implements OnInit,OnDestroy {
   navigationSubscription: Subscription;
@@ -25,12 +26,92 @@ export class RegionesPpalPage implements OnInit,OnDestroy {
 
   constructor(private regionesSvc:RegionService,
     private alertController:AlertController,
-    private router: Router,private cdr: ChangeDetectorRef) { }
+    private router: Router,private cdr: ChangeDetectorRef) { 
+      this.mensaje = 'Estoy en el constructor';
+
+      this.navigationSubscription = this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.leerRegiones();
+      }); 
+    }
 
   ngOnInit() {
     console.log('Entré a regiones en OnInit');
   }
+  ngOnDestroy(): void {
+    if (this.navigationSubscription) {
+      this.navigationSubscription.unsubscribe();
+    }
+  }
 
+  leerRegiones(){
+    this.regionesSvc.dameListaRegiones().subscribe({
+      next:(res:any)=>{
+        console.log('Servicio leido de forma exitosa')
+        console.log(res);
+        this.regiones=res;
+        
+        for(let region of this.regiones){
+          region.hola='Hola';
+        }
+        console.log(this.regiones);
+        this.regiones 
+        this.cdr.detectChanges();
+
+      },
+      error:(error:any)=>{
+        console.log('Error en la lectura del servicio')
+        console.log(error)
+
+      }
+    })
+  }
+  borraRegion(id:string){
+    console.log('Voy a borrar esta región='+id);
+
+    this.regionesSvc.borraRegion(id).subscribe({
+      next:(res:any)=>{
+        console.log('Región borrada de forma exitosa')
+        console.log(res);
+        this.leerRegiones();
+        
+
+      },
+      error:(error:any)=>{
+        console.log('Error en el borrado de la región')
+        console.log(error)
+
+      }
+    })
+
+  }
+  async confirmaBorrar(region:Region){
+    const alert = await this.alertController.create({
+      header: 'Confirmación',
+      message: '¿Estás seguro de que deseas borrar la región '+region.nombreRegion+' ?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Operación cancelada');
+          }
+        },
+        {
+          text: 'Aceptar',
+          handler: () => {
+            console.log('Operación confirmada');
+            this.borraRegion(region.idRegion);
+            // Aquí puedes agregar la lógica para la operación a realizar
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+
+  }
   saltaAInsertarRegion() {
     this.router.navigateByUrl('/insertar-region');
   }  
